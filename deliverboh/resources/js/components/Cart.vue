34ac019b0af899200de1f942f3cb71585c39224a
Davide <dadavidenapolitano@gmail.com>
2021-12-12 18:05:49 +0100

    ancora per luigi

--- deliverboh/resources/js/components/Cart.vue
+++ deliverboh/resources/js/components/Cart.vue
@@ -107,6 +107,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.panel-footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+    background: seagreen;
+}
               
     .cart {
         position: relative;
